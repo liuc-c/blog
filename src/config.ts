02ad3780f81032aaea2e210d4682a112f181a27a
liuc-c <408315942@qq.com>
2025-07-06 13:48:14 +0800

    更新网站图标，修改 favicon 和 apple-touch-icon，更新 SVG 文件以支持深色模式，调整网站配置中的副标题为“仟仟の自留地”。

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "浮生仟记",
-	subtitle: "仟仟的自留地",
+	subtitle: "仟仟の自留地",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 180, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
